69477a7526cec9ad32bf6a2111ea456c4fa9ab1e
nnoce14 <nnoce12@gmail.com>
2022-05-16 15:20:03 -0400

    Refactored properties listing to use Card component from antd

--- src/components/editor/components/properties-listing.tsx
+++ src/components/editor/components/properties-listing.tsx
@@ -1,37 +1,50 @@
+import { Link } from 'react-router-dom';
 import { gql, useQuery } from '@apollo/client';
 import { useNode } from '@craftjs/core';
-import { useParams } from 'react-router-dom';
+import { Typography, Card, Space } from 'antd';
+
+const { Text, Title } = Typography;
 
 const GET_PROPERTIES_BY_COMMUNITY = gql`
     query PropertiesByCommunity($communityId: ID!) {
         propertiesByCommunityId(communityId: $communityId) {
             propertyName
             propertyType
+            owner {
+                memberName
+            }
             location {
                 address {
                     streetNumber
                     streetName
                 }
             }
-            owner {
-                memberName
-            }
+            listedForSale
+            listedForRent
+            listedForLease
+            listedInDirectory
         }
     }
 `;
 
+interface PropertiesListingProps {
+    data: {
+        communityId: string;
+    }
+}
+
 let PropertiesListing: any;
 
-PropertiesListing = () => {
+PropertiesListing = (props: PropertiesListingProps) => {
     const { connectors: { connect, drag }, selected } = useNode((state) =>(
         {
         selected: state.events.selected
     }));
 
-    const params = useParams();
-
     const { loading, error, data } = useQuery(GET_PROPERTIES_BY_COMMUNITY, {
-        variables: { communityId: params.communityId },
+        variables: { 
+            communityId: localStorage.getItem('community') ?? '' 
+        },
     });
 
     const content = () => {
@@ -42,14 +55,15 @@ PropertiesListing = () => {
                 className="px-4 py-2"
                 ref={ref => connect(drag(ref as HTMLDivElement))} 
             >
-                <div className="bg-white" style={{display:'flex', justifyContent: 'center' }}>
+                <div className="bg-white shadow overflow-hidden sm:rounded" style={{display:'flex', justifyContent: 'space-around', }}>
                     {data && data.propertiesByCommunityId && data.propertiesByCommunityId.map((property: any) => (
-                    <div className="bg-white shadow overflow-hidden sm:rounded-lg" style={{ margin: '0% 2.5%'}}>
-                        <h2>Name: {property.propertyName}</h2>
-                        <p>Type: {property.propertyType}</p>
-                        {property.location && <p>Location: {property.location.address.streetNumber} {property.location.address.streetName}</p>}
-                        <p>Owner: {property.owner.memberName}</p>
-                    </div>))}
+                        <Card title={<Title level={4}>{property.propertyName}</Title>} size='small' style={{ margin: '15px 0'}} extra={<Link to='/:id'></Link>}>
+                            <Space direction='vertical' size='small'>
+                                {property.owner && <Text italic>Owner - {property.owner.memberName}</Text>}
+                                {property.propertyType && <Text>Property Type - {property.propertyType}</Text>}
+                            </Space>
+                        </Card>
+                    ))}
                 </div>
             </div>
         )
@@ -60,7 +74,15 @@ PropertiesListing = () => {
     </>
 }
 
-PropertiesListing.craft = {}
+const PropertiesListingSettings = () => {
+    return <></>
+}
+
+PropertiesListing.craft = {
+    related: {
+        settings: PropertiesListingSettings
+    }
+}
 
 export {
     PropertiesListing
--- src/components/editor/page/toolbox.tsx
+++ src/components/editor/page/toolbox.tsx
@@ -3,6 +3,7 @@ import { Button } from 'antd';
 import { useEditor } from '@craftjs/core';
 
 import * as CmsComponents from "../components/";
+import { useParams } from 'react-router-dom';
 
 interface EditorConfigDefinition {
   categories: {
@@ -16,6 +17,7 @@ interface EditorConfigDefinition {
 
 export const Toolbox: React.FC<any> = (props) => {
   const { connectors, query } = useEditor();
+  const params = useParams();
 
   const editorConfig:EditorConfigDefinition = {
     categories: [
@@ -71,7 +73,7 @@ export const Toolbox: React.FC<any> = (props) => {
           },
           {
             name: 'Properties Listing',
-            component: <CmsComponents.PropertiesListing />
+            component: <CmsComponents.PropertiesListing data={{ communityId: localStorage.getItem('community')}} />
           }
         ]
       }
